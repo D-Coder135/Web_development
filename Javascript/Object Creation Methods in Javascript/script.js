78fad7aae695725137a1f33223eb87b2bd6a7294
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-10-26 00:01:18 +0530

    Log the output on console using dot(.) method (object.objectproperty)

--- Javascript/Object Creation Methods in Javascript/script.js
+++ Javascript/Object Creation Methods in Javascript/script.js
@@ -4,4 +4,7 @@ company.name = "Facebook";
 company.ceo = new Object();
 company.ceo.firstName = "Mark";
 company.ceo.favColor = "blue";
+
+
 console.log(company);
+console.log("Company CEO name is: " +company.ceo.firstname);
