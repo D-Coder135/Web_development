494a0787985440bcb90ada609acb8cb0e55ed1a2
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-10-26 00:05:57 +0530

    Log the output on console using method other than dot method

--- Javascript/Object Creation Methods in Javascript/script.js
+++ Javascript/Object Creation Methods in Javascript/script.js
@@ -10,5 +10,9 @@ console.log(company);
 console.log("Company CEO name is: " +company.ceo.firstname);
 
 console.log(company["name"]);
+
+
 var stockPropName = "stock of company";
 company[stockPropName] = 110;
+
+console.log("Stock price is: " + company[stockPropName]);
