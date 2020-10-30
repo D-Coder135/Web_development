e45565ecd8c3fd55ff8405187ff59dc757f3c7bb
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-10-30 19:53:00 +0530

    Add text and example for a new concept in arrays in JavaScript

--- Javascript/Arrays In JS/script.js
+++ Javascript/Arrays In JS/script.js
@@ -25,3 +25,9 @@ console.log(names);
 for (var i = 0; i < names.length; i++) {
   console.log("Hello " + names[i]);
 }
+
+// Another concept!
+names[100] = "Kimiko";
+for (var i = 0; i < names.length; i++) {
+  console.log("Hello " + names[i]);
+}
