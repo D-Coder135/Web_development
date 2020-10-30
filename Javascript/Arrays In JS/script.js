9a5319430c77bbe0a4a2014033d3ab1e6011cb04
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-10-30 19:41:55 +0530

    Initialize array object with primitive, String, function and obj literal

--- Javascript/Arrays In JS/script.js
+++ Javascript/Arrays In JS/script.js
@@ -2,3 +2,9 @@
 
 // Array creation (Long Hand Notation)
 var array = new Array();
+array[0] = "Devansh";
+array[1] = "19";
+array[2] = function (name) {
+  console.log("Hello " + name);
+};
+array[3] = { Course: " HTML, CSS & JS"};
