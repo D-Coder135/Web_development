6a2f3ff4618b44fdd00d01e0672b3d972dd6a952
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-01 00:02:46 +0530

    Add contents as an example of IIFE in script2.js

--- Javascript/Namespaces In JavaScript/script2.js
+++ Javascript/Namespaces In JavaScript/script2.js
@@ -3,11 +3,18 @@ function sayHi() {
   console.log("Hi " + name);
 } */
 
-
+/*
 
 var omiGreeter = {};
 omiGreeter.name = "Omi";
 var greeting = "Hi ";
 omiGreeter.sayHi = function () {
   console.log(greeting + omiGreeter.name);
+}
+
+*/
+
+// IIFE
+(function (window)) {
+  
 }
