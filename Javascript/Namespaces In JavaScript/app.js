9795d3f2a89877ff113ce73c0d1a0c68718b0c4d
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-10-31 18:20:44 +0530

    Add some important text info for description

--- Javascript/Namespaces In JavaScript/app.js
+++ Javascript/Namespaces In JavaScript/app.js
@@ -1,2 +1,6 @@
 sayHello();
 sayHi();
+// The output we get on the console is not what we have desired to get.
+// Because the same varible name is defined twice with different values,
+// therefore there occurs redundancy.
+// Hence here comes Namespaces in context.
