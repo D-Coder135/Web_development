6b2568929894d70d1be28e18cb437b536e473b4d
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-10-31 18:30:10 +0530

    Call both the properties defined previously inside app.js to get output. Hence, we get the desired output.

--- Javascript/Namespaces In JavaScript/app.js
+++ Javascript/Namespaces In JavaScript/app.js
@@ -1,6 +1,10 @@
-sayHello();
-sayHi();
-// The output we get on the console is not what we have desired to get.
-// Because the same varible name is defined twice with different values,
-// therefore there occurs redundancy.
-// Hence here comes Namespaces in context.
+//sayHello();
+//sayHi();
+
+/* The output we get on the console is not what we have desired to get.
+  Because the same varible name is defined twice with different values,
+  therefore there occurs redundancy.
+ Hence here comes Namespaces in context. */
+
+devanshGreeter.sayHello();
+omiGreeter.sayHi();
