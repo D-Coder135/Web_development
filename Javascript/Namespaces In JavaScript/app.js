f1526d416abd9777b228ba866d0cda1073d513b0
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-10-31 23:56:38 +0530

    Add some important text info in app.js about the new concept IIFE in JS

--- Javascript/Namespaces In JavaScript/app.js
+++ Javascript/Namespaces In JavaScript/app.js
@@ -8,3 +8,11 @@
 
 devanshGreeter.sayHello();
 omiGreeter.sayHi();
+
+/* Now after declaring a global variable greetingin both script1.js and script2.
+js, the output which we will get on the console will not be the desired output,
+as the scope of the varible greeting will be global and hence redundancy will
+take place. */
+
+/* Therefore, to overcome or remove this error a new concept comes in context
+known as "Imediately Invoked Function Expression (IIFE)". */
