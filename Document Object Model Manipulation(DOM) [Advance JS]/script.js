847d5862670d3cc919eb41da40617cbc60e883ef
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-01 22:51:00 +0530

    Create a function name sayHello

--- Document Object Model Manipulation(DOM) [Advance JS]/script.js
+++ Document Object Model Manipulation(DOM) [Advance JS]/script.js
@@ -8,3 +8,8 @@ the heading Part having Id title from the html code index.html */
 // console.log(document instanceOf HTMLDocument);
 /* Showing that the document object is an instance of HTMLDocument and therefore
 it can be used as a global object inside a JavaScript file. */
+
+
+function sayHello() {
+  
+}
