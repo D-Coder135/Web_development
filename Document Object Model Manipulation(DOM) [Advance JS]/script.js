ea2475190b0bf9433e879c359093ce6dfdd1de50
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-01 22:45:33 +0530

    Add important text info for description

--- Document Object Model Manipulation(DOM) [Advance JS]/script.js
+++ Document Object Model Manipulation(DOM) [Advance JS]/script.js
@@ -1,2 +1,4 @@
 // DOM Manipulation
 console.log(document.getElementById("title"));
+/* Using document object along with the property getElementById to extract
+the heading Part having Id title from the html code index.html */
