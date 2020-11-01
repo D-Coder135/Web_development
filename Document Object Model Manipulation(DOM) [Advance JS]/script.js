a22146c27e3444ebc4269cf68019724ebc8784c3
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-01 22:49:15 +0530

    Add important text info for description.

--- Document Object Model Manipulation(DOM) [Advance JS]/script.js
+++ Document Object Model Manipulation(DOM) [Advance JS]/script.js
@@ -2,4 +2,7 @@
 console.log(document.getElementById("title"));
 /* Using document object along with the property getElementById to extract
 the heading Part having Id title from the html code index.html */
+
 console.log(document instanceOf HTMLDocument);
+/* Showing that the document object is an instance of HTMLDocument and therefore
+it can be used as a global object inside a JavaScript file. */
