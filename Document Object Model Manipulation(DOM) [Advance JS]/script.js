9bffb2f521185c5da351b01b66603c9dced03608
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-01 22:47:49 +0530

    showing result as true/false on console. Proving that the document object is an instance of HTMLDocument and therefore used as a global object.

--- Document Object Model Manipulation(DOM) [Advance JS]/script.js
+++ Document Object Model Manipulation(DOM) [Advance JS]/script.js
@@ -2,3 +2,4 @@
 console.log(document.getElementById("title"));
 /* Using document object along with the property getElementById to extract
 the heading Part having Id title from the html code index.html */
+console.log(document instanceOf HTMLDocument);
