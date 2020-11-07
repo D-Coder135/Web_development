031f9e08629b34417968b94335395ef9f6fba042
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-07 19:57:26 +0530

    Add previously created contents inside the function in addEventListener

--- Handling events using JS/script.js
+++ Handling events using JS/script.js
@@ -5,6 +5,31 @@ image or extra part is loaded. */
 
 document.addEventListener("DOMContentLoaded",
   function (event) {
-    
+    function sayHello (event) {
+      this.textContent = "Said it!";
+      var name =
+       document.getElementById("name").value;
+       var message = "<h2>Hello " + name + "!</h2>";
+
+      document
+        .getElementById("content")
+        .innerHTML = message;
+
+      if (name === "student") {
+        var title =
+          document
+            .querySelector("#title")
+            .textContent;
+        title += " & Lovin' it!";
+        document
+            .querySelector("h1")
+            .textContent = title;
+      }
+    }
+
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", sayHello);
+
   }
 );
