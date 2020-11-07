e53c9944d5604602a16f330271897a76877f88d3
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-07 19:55:26 +0530

    Add some text info

--- Handling events using JS/script.js
+++ Handling events using JS/script.js
@@ -1,4 +1,8 @@
 // Event Handling
+
+/* Using the DOMContentLoaded we can load the contents of the code before any
+image or extra part is loaded. */
+
 document.addEventListener("DOMContentLoaded",
 
 );
